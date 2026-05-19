1b89712531fc4e915f54863e2c17da1b4b1f97df
James Cuzella <james.cuzella@lyraphase.com>
2026-05-19 00:57:40 -0600

    Initial XCode project template

---
+++ PhotoanalysisdHydraKiller/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PhotoanalysisdHydraKiller
+//
+//  Created by James Cuzella on 5/19/26.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
